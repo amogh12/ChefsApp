5bb90d143c039b483853e5085f4c39e83e93b858
DATB01\E88311 <aphadke@atb.com>
2018-09-24 13:47:41 -0600

    added two more API calls to post and delete data

--- routes/api/items.js
+++ routes/api/items.js
@@ -2,10 +2,34 @@ const express = require('express');
 const router = express.Router();
 const Item = require("../../models/Item");
 
+
+//@route GET api/items
+//@access Public
+//@desc get all items
 router.get('/', (req, res) => {
     Item.find()
         .sort({date: -1}) //sort by date order is descending
         .then(items => res.json(items));
+});
+
+//@route POST api/items
+//@access Public
+//@desc post data in the MongoDB
+router.post('/', (req, res) => {
+    const newItem = new Item ({
+        name:req.body.name
+    });
+
+    newItem.save().then(item => res.json(item));
+});
+
+//@route delete api/items/id
+//@access Public
+//@desc delete data in the MongoDB
+router.delete('/:id', (req, res) => {
+    Item.findById(req.params.id)
+        .then(item => item.remove().then(() => res.json({success: true})))
+        .catch(err => res.status(404).json({success: false}));
 });
 
 module.exports = router;
